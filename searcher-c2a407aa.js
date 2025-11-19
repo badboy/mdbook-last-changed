023fb64470b775d1ef90847b54c7b33efee7b520
badboy <badboy@users.noreply.github.com>
2025-11-19 15:07:50 +0000

    deploy: 13dac567068c8ab9c4f7194de77321d965b5a07c

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d445c44c.js',
+                path_to_root + 'searchindex-c427b5e3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
